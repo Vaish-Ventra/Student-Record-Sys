2339a761cb23b62d23e5c904fb2349583e246d18
Vaish-Ventra <ssvaishnavi.ventra@gmail.com>
2024-09-26 15:29:26 +0530

    Update MyPortal.py

--- MyPortal.py
+++ MyPortal.py
@@ -5,11 +5,10 @@ Created on Wed Aug  7 12:43:30 2024
 @author: ssvai
 """
 import mysql.connector as sqltor
-from mysql.connector import errorcode
 
 def HomePg():
     while True:
-        usertype= int(input("Enter 1 for student login and 2 for admin login. "))
+        usertype= int(input("Enter 1 for student and 2 for admin. "))
         if usertype==1:
             print ("\n\nStudent Login")
             StudentLogin()
@@ -57,13 +56,11 @@ def AdminLogin():
                     print ("You have logged in.")
                     #Next pg
                     while True:
-                        x= int(input("Enter 1 to add courses, 2 to add marks, 3 to update password, anything else to exit"))
+                        x= int(input("Enter 1 to add courses, 2 to add marks, anything else to exit"))
                         if x==1:
                             AddCourse()
                         elif x==2:
                             AddMarks()
-                        elif x==3:
-                            UpdatePassword(A_ID, 2)
                         else:
                             print('Exited')
                             break
@@ -145,7 +142,6 @@ def AddMarks():
 
 def StudentLogin():
     mycon= EstablishConnection()
-    cursor= mycon.cursor()
     
     if mycon!=None:
         backtohomepg= False
@@ -153,6 +149,7 @@ def StudentLogin():
             S_ID= int( input("Enter ID: "))
             Password= input("Enter Password: ")
             
+            cursor= mycon.cursor()
             st= "SELECT * FROM sidnp;"
             cursor.execute(st)
             
@@ -165,36 +162,22 @@ def StudentLogin():
                         backtohomepg=True
                     break
                 elif row[0]==S_ID and row[1]==Password:
-                    print("You have logged in.\n \n \n Veiw Marks(Press 1)\t\t Update Password(Press 2) ")
-                    VorU= Selection()
-                    if VorU==1:
-                        CheckAnotherSem= True
-                        while CheckAnotherSem== True:
-                            ViewMarks(S_ID)
-                            x= int( input("To return to home pg type 10, to check another semesters marks type 20: "))
-                            if x==10:
-                                backtohomepg=True
-                                CheckAnotherSem=False
-                        break
-                    else:
-                        UpdatePassword(S_ID, 1)
-                        backtohomepg=True
-                        break
+                    print ("You have logged in.\n \n \n Veiw Marks")
+                    CheckAnotherSem= True
+                    while CheckAnotherSem== True:
+                        ViewMarks(S_ID)
+                        x= int( input("To return to home pg type 10, to check another semesters marks type 20: "))
+                        if x==10:
+                            backtohomepg=True
+                            CheckAnotherSem=False
+                    break
                     
         mycon.close()
 
-def Selection():
-    x= int(input())
-    if x==1 or x==2:
-        return x
-    else:
-        print("Enter 1 or 2!")
-        Selection()
 
 def CalcSPI(ID, batch, branch, sem):
     mycon= EstablishConnection()
     cursor=mycon.cursor()
-    
     st= "SELECT * FROM {}_{}_{} WHERE StudentID={};".format(batch,branch,sem,ID)
     cursor.execute(st)
     marks= cursor.fetchone()
@@ -206,7 +189,7 @@ def CalcSPI(ID, batch, branch, sem):
     Sum=0
     for i in range (1, len(marks)):
         if marks[i]==None:
-            print("U have a backlog in ", data[i][0])
+            print("U have a backlog in ",data[i][0], "in sem ", sem)
         else:
             Sum +=marks[i]
     SPI= Sum/ (len(marks)-1)
@@ -234,73 +217,48 @@ def CalcCPI(ID, CurrentSem, batch, branch):
 
 def ViewMarks(ID):
     mycon= EstablishConnection()
-    if mycon!=None:
-        cursor=mycon.cursor()
-        st1= "SELECT * FROM studentinfo WHERE StudentID= {}".format(ID)
-        cursor.execute(st1)
-        s_info= cursor.fetchone()
-        name= s_info[1]
-        branch= s_info[2]
-        batch= s_info[3]
-        CurrentSem= s_info[4]
-        #print details
-        print ("ID- ", ID)
-        print ("Name- ", name)
-        print ("Branch- ", branch)
-        print ("Batch- ", batch)
-        print ("Current sem- ", CurrentSem)
-        
-        sem= input("Which SEM'S marks do you want to check. Enter NUMBER: ")
-        
-        try:
-            st= "SELECT * FROM {}_{}_{} WHERE StudentID={};".format(batch,branch,sem,ID)
-            cursor.execute(st)
-            marks= cursor.fetchone()
-            
-            st2= "DESC {}_{}_{};".format(batch, branch, sem)
-            cursor.execute(st2)
-            data= cursor.fetchall()
-            
-            for i in range (1, len(marks)):
-                print (data[i][0], ":", marks[i])
-                
-            print( "SPI : ", CalcSPI(ID, batch, branch, sem))
-            if CalcCPI(ID, CurrentSem, batch, branch)==-1:
-                print ("No CPI assigned yet as you are still in your first sem")
-            else:
-                print ("CPI: ", CalcCPI(ID, CurrentSem, batch, branch))
-        except sqltor.Error as err:
-            if err.errno == errorcode.ER_NO_SUCH_TABLE:
-                print ("You haven't reached that sem yet!")
-            else:
-                print (err)
-        finally:
-            mycon.close()
-
-def UpdatePassword(ID, SorP):
-    mycon= EstablishConnection()
     cursor=mycon.cursor()
-    Password= input("Enter new Password. it must be atleast 3 char long. ")
-    if SorP==1:
-        st= "UPDATE sidnp SET Password='{}' WHERE StudentID={};". format(Password, ID)
-        cursor.execute(st)
-        mycon.commit()
-        print("Password Updated")
+    st1= "SELECT * FROM studentinfo WHERE StudentID= {}".format(ID)
+    cursor.execute(st1)
+    s_info= cursor.fetchone()
+    studID= s_info[0]
+    name= s_info[1]
+    branch= s_info[2]
+    batch= s_info[3]
+    CurrentSem= s_info[4]
+    #print details
+    print ("ID- ", ID)
+    print ("Name- ", name)
+    print ("Branch- ", branch)
+    print ("Batch- ", batch)
+    print ("Current sem- ", CurrentSem)
+    
+    sem= input("Which SEM'S marks do you want to check. Enter NUMBER: ")
+    
+    st= "SELECT * FROM {}_{}_{} WHERE StudentID={};".format(batch,branch,sem,ID)
+    cursor.execute(st)
+    marks= cursor.fetchone()
+    
+    st2= "DESC {}_{}_{};".format(batch, branch, sem)
+    cursor.execute(st2)
+    data= cursor.fetchall()
+    
+    for i in range (1, len(marks)):
+        print (data[i][0], ":", marks[i])
+        
+    print( "SPI : ", CalcSPI(ID, batch, branch, sem))
+    print ("-------\t--------\t--------\t--------\t-------")
+    CPI= CalcCPI(ID, CurrentSem, batch, branch)
+    if CPI==-1:
+        print ("No CPI assigned yet as you are still in your first sem")
     else:
-        st= "UPDATE aidnp SET Password='{}' WHERE AdminID={};". format(Password, ID)
-        cursor.execute(st)
-        mycon.commit()
-        print("Password Updated")
-   
+        print ("CPI: ", CPI)
     mycon.close()
-     
-
-
-
-#Create student sign up, update password for both admins an students   
+            
+#Create student sign up, UPDATE password for both admins an students   
 
 HomePg()
-                    
+
         
     
         
